58b14651633a4cd9ed9a89742254d5ed7a5af6f0
hunterxcobby <solomonsefah13@gmail.com>
2023-08-27 23:26:56 +0000

    removed , 'asking for user's name' when starting.

--- menu.c
+++ menu.c
@@ -13,20 +13,8 @@ int input;
 
 void display_menu(void)
 {
-	char name[100];
-	int n;
 
 	printf(">> Hello, Welcome to simple calculator\n");
-	printf(">> What is you name?\n");
-	fgets(name, sizeof(name), stdin);
-
-	for(n = 0; name[n] != '\0'; n++)
-	{
-		name[n] = toupper(name[n]);
-	}
-	name[strcspn(name, "\n")] = '\0';
-
-	printf("\nOkay, %s ! \n\n", name);
 	printf("\x1b[4mThis is the MENU of operations you can perform with me.\x1b[0m\n");
 	printf("\n\x1b[4mPress:\x1b[0m\n");
 	printf("1: for ADDITION\n");
